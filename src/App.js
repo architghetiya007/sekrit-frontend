a3942bc028482f90cc2a4f2b479db0889c22accd
Archit Ghetiya <ghetiyarchit007@gmail.com>
2024-12-22 23:11:35 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ConfirmLogin from "./Components/ConfirmLogin.js";
 import Loginform from "./Components/Loginform.js";
+import Home from "./Components/Home.js";
+import Coverage from "./Components/Coverage.js";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Loginform />} />
         <Route path="/confirm-login" element={<ConfirmLogin />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/coverage" element={<Coverage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Loginform.js
+++ src/Components/Loginform.js
@@ -3,22 +3,25 @@ import "./Loginform.css"; // Import the styles
 import Logo from "./../Images/logo.png";
 import { MdMailOutline } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
   return (
     <div className="login-container">
-      <img src={Logo} className="logo" />
+      <img src={Logo} className="logo" alt="logo"/>
       <p className="subtitle">Sign In to Sekrit</p>
       <div className="login-box">
         <div className="login-box-content">
           <div className="login-options">
-            <button className="login-btn email-btn"><MdMailOutline />Login with E-mail</button>
+            <button className="login-btn email-btn">
+              <Link to="/confirm-login">
+              <MdMailOutline />Login with E-mail</Link></button> 
             <button className="login-btn github-btn"><FaGithub />Login with GitHub</button>
           <p className="policy-text">
             By logging in, you agree to Sekrit's <a href="#">Privacy Policy</a>{" "}
             and <a href="#">Terms of Use</a>.
           </p>
-
+          
           <p className="signup-text">
             <p>Don't have an account yet? </p><br />
             <a href="#" className="signup-link">Sign up for a free trial</a>
